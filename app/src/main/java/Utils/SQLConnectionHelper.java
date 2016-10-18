e76ebaf6dc86b39a16a7bf6f49bbcf0c8a55c1f3
duffdean <dean@bmc-ne.co.uk>
2016-10-18 22:04:45 +0100

    SQL Connection util added. SQLite to SQL missing method body

---
+++ app/src/main/java/Utils/SQLConnectionHelper.java
@@ -0,0 +1,44 @@
+package Utils;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ * Created by dean on 18/10/16.
+ */
+
+public class SQLConnectionHelper {
+    String ip = "192.168.0.100";
+    String classs = "net.sourceforge.jtds.jdbc.Driver";
+    String db = "Andro";
+    String un = "hitesh";
+    String password = "789";
+
+    @SuppressLint("NewApi")
+    public Connection CONN()
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+
+            Class.forName(classs);
+            ConnURL = "jdbc:jtds:sqlserver://" + ip + ";"
+                    + "databaseName=" + db + ";user=" + un + ";password="
+                    + password + ";";
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+}
